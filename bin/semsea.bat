44a883c4f57bbc1ecaafbad95a0196190c51e6c5
codrshi <pjdrd24680@gmail.com>
2026-06-08 23:10:46 +0530

    move scripts to bin folder

--- bin/semsea.bat
+++ bin/semsea.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set "JAR=%~dp0semsea.jar"
-if not exist "%JAR%" set "JAR=%~dp0target\semsea.jar"
+if not exist "%JAR%" set "JAR=%~dp0..\target\semsea.jar"
 if not exist "%JAR%" (
     echo semsea.jar not found.
     echo Build it first with: mvn package
